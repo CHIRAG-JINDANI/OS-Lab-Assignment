85346249c2871d12ce851779ca29a737ffb516c1
chiraged <95534609+CHIRAG-JINDANI@users.noreply.github.com>
2026-04-10 16:56:31 +0530

    Update thread_addresses.c

--- Program1_Threads/thread_addresses.c
+++ Program1_Threads/thread_addresses.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
-
-// SHARED: Everyone sees the same global variable
 int global_var = 100;
 
 void* thread_work(void* arg) {
     int id = *(int*)arg;
     
-    // PRIVATE: Each thread gets its own 'local_var' on its own stack
     int local_var = id * 10;
 
     printf("Thread %d: Global Addr: %p | Local Addr: %p\n", id, &global_var, &local_var);
@@ -22,12 +19,10 @@ int main() {
 
     printf("Main:     Global Addr: %p\n\n", &global_var);
 
-    // Create 3 threads
     for (int i = 0; i < 3; i++) {
         pthread_create(&threads[i], NULL, thread_work, &ids[i]);
     }
 
-    // Wait for them to finish
     for (int i = 0; i < 3; i++) {
         pthread_join(threads[i], NULL);
     }
